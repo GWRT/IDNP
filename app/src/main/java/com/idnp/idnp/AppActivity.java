65d08c63af67fd714c947880893cee13d4a5d190
GWRT <gramosti@unsa.edu.pe>
2022-12-11 19:14:42 -0500

    avance del mapa de google

--- app/src/main/java/com/idnp/idnp/AppActivity.java
+++ app/src/main/java/com/idnp/idnp/AppActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 public class AppActivity extends AppCompatActivity {
 
+    MapsFragment mapsFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
+
+        mapsFragment = new MapsFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, mapsFragment).commit();
     }
 }
---
+++ app/src/main/java/com/idnp/idnp/MapsFragment.java
@@ -0,0 +1,48 @@
+package com.idnp.idnp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsFragment extends Fragment {
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            LatLng plazaArmas = new LatLng(-16, -71);
+            googleMap.addMarker(new MarkerOptions().position(plazaArmas).title("Plaza de Armas"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(plazaArmas,10));
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_maps, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+}
